4e0a41e1e19970a6c19d4b841ef64eae76a6aeb3
helenaraimundo91 <139161478+helenaraimundo91@users.noreply.github.com>
2023-11-06 20:28:51 +0000

    Landing page almoust finished

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div className='container-fluid text-center bg-dark text-white p-4 mt-5'>
+      Copyright @ Your Website 2023
+    </div>
+  )
+}
+
+export default Footer
+
---
+++ src/js/component/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import NavItem from './NavItem'
+
+
+const Navbar = () => {
+  return (
+    <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+      <div className='container-fluid'>
+        <a className='navbar-brand' href='#'>
+          Start Bootstrap
+        </a>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarText'
+          aria-controls='navbarText'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
+        <div
+          className='collapse navbar-collapse flex-row-reverse'
+          id='navbarText'>
+            <NavItem/>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
